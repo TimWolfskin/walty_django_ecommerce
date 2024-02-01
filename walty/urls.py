7ffed995298fb05fa7c548fb566a980c6333df3e
TimWolfskin <timwolfskin013@gmail.com>
2024-02-01 15:08:47 +0300

    added user registeration and authentication

--- walty/urls.py
+++ walty/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include('core.urls')),
+    path('user/', include('userauths.urls'))
 ]
 
 if settings.DEBUG:
