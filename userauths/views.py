57274825bf522df70e086b99e1249c08ba97d7c3
TimWolfskin <timwolfskin013@gmail.com>
2024-02-01 17:28:02 +0300

    Added login functionality

--- userauths/views.py
+++ userauths/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from userauths.forms import UserRegisterForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 
@@ -26,3 +29,33 @@ def register_view(request):
         'form': form,
     }
     return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        messages.warning(request, f"You already logged in")
+        return redirect("core:index")
+    
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.warning(request, f"User with {email} does not exist.")
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in.")
+            return redirect("core:index")
+        else:
+            messages.warning(request, "User does not exist, create an account.")
+    
+    context = {
+
+    }
+
+    return render(request, "userauths/sign-in.html", context)
