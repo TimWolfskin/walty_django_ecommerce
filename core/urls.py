75d127105744ddafee1a30fd01d79b67e1fbdded
TimWolfskin <timwolfskin013@gmail.com>
2024-02-02 21:45:15 +0300

    added product list

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from core.views import index
+from core.views import index, product_list_view
 
 app_name = "core"
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('products/', product_list_view, name="product-list")
 ]
--- core/views.py
+++ core/views.py
@@ -10,3 +10,13 @@ def index(request):
         "products": products
     }
     return render(request, 'core/index.html', context)
+
+
+
+def product_list_view(request):
+    products = Product.objects.filter(product_status="published", featured=True)
+
+    context = {
+        "products": products
+    }
+    return render(request, 'core/product-list.html', context)
