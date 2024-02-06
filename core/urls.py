7c1f4915af6680abdbe643d692d7c0c57cf085f0
TimWolfskin <timwolfskin013@gmail.com>
2024-02-06 16:19:25 +0300

    added search functional

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django import views
 from django.urls import path
-from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_list_view, vendor_detail_view, product_detail_view, tag_list, ajax_add_review
+from core.views import index, product_list_view, category_list_view, category_product_list_view, vendor_list_view, vendor_detail_view, product_detail_view, tag_list, ajax_add_review, search_view
 
 app_name = "core"
 
@@ -17,5 +17,6 @@ urlpatterns = [
 
     path('products/tag/<slug:tag_slug>/', tag_list, name="tags"),
 
-    path('ajax-add-review/<int:pid>/', ajax_add_review, name='ajax-add-review')
+    path('ajax-add-review/<int:pid>/', ajax_add_review, name='ajax-add-review'),
+    path('search', search_view, name="search")
 ]
